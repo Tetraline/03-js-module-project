d40b9ccab013743aaf61e5815c6253f2d31ed7bb
tetraline <danielsamways@gmail.com>
2022-06-10 11:03:55 +0100

    all tests passed

--- challenge.js
+++ challenge.js
@@ -1,5 +1,45 @@
-let hello = () => {
-  return "hello world";
+let isNumberPositive = (n) => {
+  // Zero is probably positive, right?
+  if (n >= 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
-module.exports = hello;
+let convertDaysToAge = (n) => {
+  return n / 365;
+};
+
+let getLargestNumber = (input) => {
+  let largest = input[0];
+  // For loop starts at 1 since we already set [0] as the largest, we don't need to check it again.
+  for (let i = 1; i < input.length; i++) {
+    if (input[i] > largest) {
+      largest = input[i];
+    }
+  }
+  return largest;
+};
+
+let getLastName = (input) => {
+  return input[input.length - 1];
+};
+
+let allNumbersPositive = (input) => {
+  let allPositive = true;
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] < 0) {
+      allPositive = false;
+    }
+  }
+  return allPositive;
+};
+
+module.exports = {
+  isNumberPositive,
+  convertDaysToAge,
+  getLargestNumber,
+  getLastName,
+  allNumbersPositive,
+};
--- challenge.spec.js
+++ challenge.spec.js
@@ -1,7 +1,53 @@
-const hello = require("./challenge");
+const c = require("./challenge");
 
-describe("Nology Pre-Course JS Challenge", function () {
-  test("Hello", function () {
-    expect(hello()).toEqual("hello world");
+describe("isNumberPositive", function () {
+  test("Negative Numbers", function () {
+    expect(c.isNumberPositive(-1)).toEqual(false);
+  });
+  test("Positive Numbers", function () {
+    expect(c.isNumberPositive(10)).toEqual(true);
+  });
+});
+
+describe("convertDaysToAge", function () {
+  test("Test 1", function () {
+    expect(c.convertDaysToAge(3650)).toEqual(10);
+  });
+  test("Test 2", function () {
+    expect(c.convertDaysToAge(6570)).toEqual(18);
+  });
+});
+
+describe("getLargestNumber", function () {
+  test("2,1,4", function () {
+    expect(c.getLargestNumber([2, 1, 4])).toEqual(4);
+  });
+
+  test("6,2,3", function () {
+    expect(c.getLargestNumber([6, 2, 3])).toEqual(6);
+  });
+});
+
+describe("getLastName", function () {
+  test("Charlie Rob Andy", function () {
+    expect(c.getLastName(["Charlie", "Rob", "Andy"])).toEqual("Andy");
+  });
+
+  test("Ash Stu", function () {
+    expect(c.getLastName(["Ash", "Stu"])).toEqual("Stu");
+  });
+});
+
+describe("allNumbersPositive", function () {
+  test("2,4,5", function () {
+    expect(c.allNumbersPositive([2, 4, 5])).toEqual(true);
+  });
+
+  test("-5,4,6", function () {
+    expect(c.allNumbersPositive([-5, 5, 6])).toEqual(false);
+  });
+
+  test("0,0,0", function () {
+    expect(c.allNumbersPositive([0, 0, 0, -1])).toEqual(false);
   });
 });
